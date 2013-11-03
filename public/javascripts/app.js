84440bfa21273af0d4698be98ba545a6b48136f0
Vincent Velociter <vincent.velociter@gmail.com>
2013-11-03 23:54:53 +0100

    Some cosmetic changes

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -6,33 +6,18 @@ $(function() {
     hScale = worldHeight / 400;
     var paper = Raphael(document.getElementById("worldmap"), worldWith, worldHeight);
     paper.rect(0, 0, worldWith, worldHeight, 10).attr({
-        stroke: "none",
-        fill: "0-#9bb7cb-#adc8da"
+        stroke: "none"
     });
-    var over = function () {
-        this.c = this.c || this.attr("fill");
-        this.stop().animate({fill: "#bacabd"}, 500);
-    },
-    out = function () {
-        this.stop().animate({fill: this.c}, 500);
-    };
     paper.setStart();
     var hue = Math.random();
     for (var country in worldmap.shapes) {
-        paper.path(worldmap.shapes[country]).attr({stroke: "#ccc6ae", fill: "#f0efeb", "stroke-opacity": 0.25}).transform("s" + wScale + "," + hScale + " 0,0");
+        paper.path(worldmap.shapes[country]).attr({stroke: "#343C40", fill: "#67777F", "stroke-opacity": 0.25}).transform("s" + wScale + "," + hScale + " 0,0");
     }
     var world = paper.setFinish();
-    world.hover(over, out);
     world.getXY = function (lat, lon) {
         return {
             cx: lon * (2.6938 * wScale) + (465.4 * wScale),
             cy: lat * (-2.6938 * hScale) + (227.066 * hScale)
-        };
-    };
-    world.getLatLon = function (x, y) {
-        return {
-            lat: (y - 227.066) / -2.6938,
-            lon: (x - 465.4) / 2.6938
         };
     };
 
@@ -51,8 +36,8 @@ $(function() {
             });
             var orig = world.getXY(data.latitude, data.longitude);
             dot.attr(orig);
-            setTimeout(function() { 
-              dot.remove(); 
+            setTimeout(function() {
+              dot.remove();
               density[densityKey]--;
             }, 1000);
             if (data.oLatitude) {
@@ -60,7 +45,8 @@ $(function() {
               var str = "M" + orig.cx + "," + orig.cy + "T" + dest.cx + "," + dest.cy;
               var line = paper.path(str);
               line.attr({
-                opacity: 0.15,
+                opacity: 0.65,
+                stroke: "#FE7727",
                 'arrow-end': 'oval-wide-long'
               });
               setTimeout(function() { line.remove(); }, 500);
